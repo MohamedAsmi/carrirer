4168b32ab934cd49d5bb7e8f84b348ce161d9aba
Asmi <shamsudeenasmi96@gmail.com>
2023-07-18 21:23:59 +0530

    new chnages request

--- database/migrations/2023_05_12_114041_create_weight_options_table.php
+++ database/migrations/2023_05_12_114041_create_weight_options_table.php
@@ -16,9 +16,7 @@ return new class extends Migration
         Schema::create('weight_options', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('region_id');
             $table->string('value')->nullable();
-            $table->foreign('region_id')->references('id')->on('regions')->onDelete('cascade');
             $table->timestamps();
         });
     }
